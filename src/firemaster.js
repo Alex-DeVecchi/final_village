59097caa4aadb589fc9589c3567459d82d494410
Alex-DeVecchi <152522811+Alex-DeVecchi@users.noreply.github.com>
2025-01-30 17:40:28 +0000

    1/30/25

--- src/firemaster.js
+++ src/firemaster.js
@@ -11,3 +11,16 @@ class FireMaster extends Update{
         this.building.applyDoubleUpgrade();
     }
 }
+
+class master1 extends Update{
+    constructor(updateName, cost, building){
+        super(updateName, cost);
+        this.building = building;
+    }
+
+    purchase() {
+        super.purchase();
+        this.building.doubleUpgrade++;
+        this.building.applyDoubleUpgrade();
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 const tickRate = 1000 / 30;
 let clickStrength = 1;
-let score = 100;
+let score = 6000;
 
 let juicer = new Building("Juicer", 0.5, 15, "buyJuicer");
 let sugar = new Building('Sugar', 2, 100, 'buySugar');
 let fireball = new Building("Fireball", 10, 500, "buyFireball");
 
 let firemaster1 = new FireMaster("Firemaster I", 100 , juicer);
+let firemaster2 = new FireMaster("Firemaster II", 500, juicer);
+let swordmaster1 = new master1("Swordmaster I", 500, sugar);
+let watermaster1 = new master1("Watermaster I", 1000, fireball);
 
 function scorePlusPlus(){
     score += clickStrength;
@@ -23,6 +26,9 @@ function updateButtons() {
     sugar.buttonState();
     fireball.buttonState();
     firemaster1.buttonState();
+    firemaster2.buttonState();
+    swordmaster1.buttonState();
+    watermaster1.buttonState();
 }
 
 function upatePage(){

5ed162ec103b7c3808434206e33bcab1a3012988
Alex-DeVecchi <152522811+Alex-DeVecchi@users.noreply.github.com>
2025-02-03 17:59:05 +0000

    2/3/2025 part 2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const tickRate = 1000 / 30;
 let clickStrength = 1;
-let score = 6000;
+let score = 60000;
 
 let slash = new Building("Slash", 0.5, 15, "buySlash");
 let waterGun = new Building('WaterGun', 2, 100, 'buyWaterGun');
@@ -8,6 +8,7 @@ let fireball = new Building("Fireball", 10, 500, "buyFireball");
 let rockThrow = new Building("RockThrow", 25, 1000,"buyRockThrow");
 let windBlow = new Building("WindBlow", 75, 2000, "buyWindBlow");
 let poisonThrow = new Building("PoisonThrow", 200, 4000,"buyPoisonThrow");
+let zap = new Building("Zap", 500, 6000, "buyZap");
 
 let swordmaster1 = new SwordMaster("Swordmaster I", 100 , slash);
 let swordmaster2 = new SwordMaster("Swordmaster II", 250, slash);
@@ -16,6 +17,7 @@ let firemaster1 = new master1("Firemaster I", 1000, fireball);
 let earthmaster1 = new master1("Earthmaster I", 1500, rockThrow);
 let airmaster1 = new master1("Airmaster I", 3000, windBlow);
 let toxicmaster1 = new master1("Toxicmaster I", 5000, poisonThrow);
+let lightningmaster1 = new master1("Lightningmaster I", 7000, zap);
 
 function scorePlusPlus(){
     score += clickStrength;
@@ -28,6 +30,7 @@ function incScore() {
     score += rockThrow.cps;
     score += windBlow.cps;
     score += poisonThrow.cps;
+    score += zap.cps;
 }
 
 function updateButtons() {
@@ -37,6 +40,8 @@ function updateButtons() {
     rockThrow.buttonState();
     windBlow.buttonState();
     poisonThrow.buttonState();
+    zap.buttonState();
+
     swordmaster1.buttonState();
     swordmaster2.buttonState();
     watermaster1.buttonState();
@@ -44,6 +49,7 @@ function updateButtons() {
     earthmaster1.buttonState();
     airmaster1.buttonState();
     toxicmaster1.buttonState();
+    lightningmaster1.buttonState();
 }
 
 function upatePage(){
